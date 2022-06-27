1cdca6588b413a4a04d85db1b78fc55af55144d5
Mateus Fonseca Lima <mateus.fonseca@fitbank.com.br>
2022-06-27 15:32:02 -0300

    Add reservations index

---
+++ HotelMVC/Controllers/ReservationsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelMVC.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Store()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Destroy()
+        {
+            return View();
+        }
+    }
+}
---
+++ HotelMVC/Models/ReservationsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelMVC.Models
+{
+    public class ReservationsModel
+    {
+        public int Id { get; set; }
+        public int IdCustomer { get; set; }
+        public int IdRoom { get; set; }
+        public int NumberOfOccupants { get; set; }
+        public DateTime ExpectedEntryDate { get; set; }
+        public DateTime ExpectedDepartureDate { get; set; }
+        public short Status { get; set; }
+    }
+}
