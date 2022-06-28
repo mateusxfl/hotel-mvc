fc3dd58af16cac60209e5286ef3b169ea14f4cc9
Mateus Fonseca Lima <mateus.fonseca@fitbank.com.br>
2022-06-28 15:24:09 -0300

    Add payment model

---
+++ HotelMVC/Models/PaymentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelMVC.Models
+{
+    public class PaymentModel
+    {
+        public int IdReservation { get; set; }
+        public double ReservationValue { get; set; }
+        public string FormOfPayment { get; set; }
+        public string Proof { get; set; }
+    }
+}
