77145e1043feaf51d808116ed7652934d3e61d22
Mateus Fonseca Lima <mateus.fonseca@fitbank.com.br>
2022-06-28 15:21:27 -0300

    Add checkin and checkout model

---
+++ HotelMVC/Models/CheckinModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelMVC.Models
+{
+    public class CheckinModel
+    {
+        public int IdReservation { get; set; }
+        public DateTime EntryDate { get; set; }
+        public DateTime EntryHour { get; set; }
+    }
+}
---
+++ HotelMVC/Models/CheckoutModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelMVC.Models
+{
+    public class CheckoutModel
+    {
+        public int IdReservation { get; set; }
+        public DateTime ExitDate { get; set; }
+        public DateTime ExitHour { get; set; }
+    }
+}
